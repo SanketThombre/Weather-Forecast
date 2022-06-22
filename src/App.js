62d686e94356abf3937434ba01b10917c51ff2d9
sanket008 <sanket.p.thombare@gmail.com>
2022-06-22 15:23:14 +0530

    Changes

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     <h1>Weather Forecast</h1> 
+      <h1>Weather Forecast</h1> 
+      <h1>Hello</h1>
     </div>
   );
 }
