240ab314e3f8ffd91a1e83ebcc0c0ab228565a4b
sanket008 <sanket.p.thombare@gmail.com>
2022-06-24 14:05:35 +0530

    working on debouncing

--- src/Components/Search.jsx
+++ src/Components/Search.jsx
@@ -1,6 +1,10 @@
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
 import { faLocationDot,faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons"
 import styled from "styled-components";
+import { useState,useEffect } from "react";
+import axios from "axios";
+
+
 
 const SearchBox = styled.div`
 width:55%;
@@ -22,13 +26,30 @@ font-size:18px;
 font-weight:bold;
 `
 export const Search = () => {
+
+    const [search, setSearch] = useState("");
+    const handleChange = (value) => {
+        // console.log(event.target);
+        // const value = event.target.value;
+
+        axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${value}&appid=10c4cbada812a8d40d0d6e944b86cc8d&units=metric`)
+            .then((res) => console.log(res))
+
+    }
+
+
     return (
+        <>
         <SearchBox>
             <div style={{fontSize:"20px"}}>
                 <FontAwesomeIcon icon={faLocationDot} /> 
                 </div>    
-            <Input type="string" placeholder="Search City" />
+            <Input type="string"  placeholder="Search City" onChange={(e)=> handleChange(e.target.value)} />
             <FontAwesomeIcon style={{fontSize:"20px"}} icon={faMagnifyingGlass} />
-        </SearchBox>
+            </SearchBox>
+
+            
+        </>
+        
     )
 }
